255580244875bd7fe652aa54fa56d94e84019b53
hsgwa <hasegawa.ats@gmail.com>
2021-10-12 23:31:29 +0900

    add: target_path_only argument

--- ros2caret/verb/callback_graph.py
+++ ros2caret/verb/callback_graph.py
@@ -36,10 +36,16 @@ class CallbackGraphVerb(VerbExtension):
             '-s', '--separate', dest='separate', type=bool, default=False,
             help='If True, split topics. default: false.')
 
+        parser.add_argument(
+            '-t', '--target_path_only', dest='target_path_only', type=bool, default=False,
+            help='Show only the path of the target. The path must be specified with --path_name.')
+
     def main(self, *, args):
         app = Application(args.architecture_path, 'yaml', None)
         callbacks = []
         if args.path_name:
             path = app.path[args.path_name]
             callbacks = path.callbacks
-        callback_graph(app._arch, callbacks, args.output_path, args.separate)
+        callback_graph(
+            app._arch, callbacks, args.output_path, args.separate, args.target_path_only
+        )

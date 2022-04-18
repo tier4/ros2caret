a7969bb6a8c69752d618ff137dca476004e66935
hsgwa <hasegawa.isp@gmail.com>
2022-04-18 14:38:18 +0900

    tag: v0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'ros2caret'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.2.1',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',

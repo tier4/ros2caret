8c152d940927d6662aeea30ceadddcfe64919ad4
hsgwa <hasegawa.isp@gmail.com>
2022-06-23 17:28:40 +0900

    pass flake8
    Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

--- ros2caret/verb/__init__.py
+++ ros2caret/verb/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.from caret_analyze import Application, Lttng
 
-from logging import Formatter, getLogger, WARNING, StreamHandler
-
 from ros2cli.plugin_system import PLUGIN_SYSTEM_VERSION
 from ros2cli.plugin_system import satisfies_version
 

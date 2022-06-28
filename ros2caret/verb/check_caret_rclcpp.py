11b4ab7674755b8cfe6b8658114a29af01c058c1
hsgwa <hasegawa.isp@gmail.com>
2022-06-28 19:48:57 +0900

    refactor: check_caret_rclcpp
    Signed-off-by: hsgwa <hasegawa.isp@gmail.com>

--- ros2caret/verb/check_caret_rclcpp.py
+++ ros2caret/verb/check_caret_rclcpp.py
@@ -21,6 +21,17 @@ from ros2caret.verb import VerbExtension
 logger = getLogger(__name__)
 logger.setLevel(INFO)
 
+class CheckCaretRclcppVerb(VerbExtension):
+
+    def add_arguments(self, parser, cli_name):
+        parser.add_argument(
+            '-w', '--workspace', dest='workspace', type=str,
+            help='the path to the workspace to be checked', required=True)
+
+    def main(self, *, args):
+        root_dir_path = args.workspace + '/build/'
+        RclcppCheck(root_dir_path)
+
 
 galactic_tp_symbol_names = [
     'ros_trace_rclcpp_publish'
@@ -36,74 +47,97 @@ caret_fork_tp_symbol_names = [
 ]
 
 
-class CheckCaretRclcppVerb(VerbExtension):
+class RclcppCheck():
 
-    def add_arguments(self, parser, cli_name):
-        parser.add_argument(
-            '-w', '--workspace', dest='workspace', type=str,
-            help='the path to the workspace to be checked', required=True)
+    def __init__(self, root_dir_path: str) -> None:
+        RclcppCheck._ensure_dir_exist(root_dir_path)
 
-    def get_package_name_from_path(self, path: str) -> str:
-        package_name = path.replace(self.root_dir_path, '').split('/')[0]
+        get_package_name = RclcppCheck._create_get_package_name(root_dir_path)
+        ros_obj_paths = RclcppCheck._get_obj_paths(root_dir_path)
 
-        return package_name
-
-    """
-    TODO: Need to speed up.
-          When this command is run against Autoware,
-          it takes approximately one minute to complete.
-    """
-    def main(self, *, args):
-        root_dir_path = args.workspace + '/build/'
-        if(os.path.exists(root_dir_path)):
-            self.root_dir_path = root_dir_path
-        else:
-            logger.error('"build" directory not found. '
-                         'Specify the path to the workspace where the build command completed.')
-            exit(1)
-
-        cmd = f'find {self.root_dir_path} -type f ! -size 0 -executable \
-                ! -name "*.o"            ! -name "*.cpp"    ! -name "*.make" \
-                ! -name "*.cmake"        ! -name "Makefile" ! -name "*.internal" \
-                ! -name "*.includecache" ! -name "*.in"     ! -name "*.txt" \
-                ! -name "*.a"            ! -name "*.stamp"  ! -name "*.genexp" \
-                ! -name "*.sample"       ! -name "*.py"'
-        filtered_file_paths = (subprocess.Popen(cmd,
-                                                stdout=subprocess.PIPE,
-                                                shell=True).communicate()[0]
-                               ).decode('utf-8').split('\n')
-
-        # Check whether at least one galactic implementation trace point exists
-        exists_galactic_tp_file_paths = []
-        for file_path in filtered_file_paths[:-1]:
-            cmd = f'nm -D {file_path} | grep -e {" -e ".join(galactic_tp_symbol_names)}'
-            out_str = (subprocess.Popen(cmd,
-                                        stdout=subprocess.PIPE,
-                                        shell=True).communicate()[0]
-                       )
-            if(out_str):
-                exists_galactic_tp_file_paths.append(file_path)
+        """
+        TODO: Need to speed up.
+            When this command is run against Autoware,
+            it takes approximately one minute to complete.
+        """
 
         # Check whether at least one CARET fork implementation trace point exists
-        all_packages = set()
-        ok_packages = set()
-        for file_path in exists_galactic_tp_file_paths:
-            all_packages.add(self.get_package_name_from_path(file_path))
-            cmd = f'nm -D {file_path} | grep -e {" -e ".join(caret_fork_tp_symbol_names)}'
-            out_str = (subprocess.Popen(cmd,
-                                        stdout=subprocess.PIPE,
-                                        shell=True
-                                        ).communicate()[0]
-                       ).decode('utf-8')
-            if(out_str):
-                ok_packages.add(self.get_package_name_from_path(file_path))
+        all_packages = {get_package_name(obj_path)
+                        for obj_path
+                        in ros_obj_paths}
+
+        ok_packages = {get_package_name(obj_path)
+                       for obj_path
+                       in ros_obj_paths
+                       if RclcppCheck._has_caret_rclcpp_tp(obj_path)}
 
         # Logging
         ng_packages = all_packages - ok_packages
-        if(ng_packages):
+        if ng_packages:
             msg = 'The following packages have not been built using caret-rclcpp:\n'
             for ng_package in ng_packages:
                 msg += f'\t{ng_package}\n'
             logger.warning(msg)
         else:
             logger.info('All packages are built using caret-rclcpp.')
+
+    @staticmethod
+    def _get_obj_paths(root_dir_path: str) -> Set[str]:
+        filtered_file_paths = RclcppCheck._get_file_paths(root_dir_path)
+
+        # Check whether at least one galactic implementation trace point exists
+        return {path
+                for path
+                in filtered_file_paths
+                if RclcppCheck._has_galactic_tp(path)}
+
+    @staticmethod
+    def _get_file_paths(dir_path: str) -> List[str]:
+        cmd = f'find {dir_path} -type f ! -size 0 -executable \
+                ! -name "*.o"            ! -name "*.cpp"    ! -name "*.make" \
+                ! -name "*.cmake"        ! -name "Makefile" ! -name "*.internal" \
+                ! -name "*.includecache" ! -name "*.in"     ! -name "*.txt" \
+                ! -name "*.a"            ! -name "*.stamp"  ! -name "*.genexp" \
+                ! -name "*.sample"       ! -name "*.py"'
+        return (subprocess.Popen(cmd,
+                                 stdout=subprocess.PIPE,
+                                 shell=True).communicate()[0]
+                ).decode('utf-8').split('\n')[:-1]
+
+    @staticmethod
+    def _has_galactic_tp(obj_path: str) -> bool:
+        cmd = f'nm -D {obj_path} | grep -e {" -e ".join(galactic_tp_symbol_names)}'
+        return (subprocess.Popen(cmd,
+                                 stdout=subprocess.PIPE,
+                                 shell=True).communicate()[0]
+                )
+
+    @staticmethod
+    def _has_caret_rclcpp_tp(obj_path: str) -> bool:
+        cmd = f'nm -D {obj_path} | grep -e {" -e ".join(caret_fork_tp_symbol_names)}'
+        return (subprocess.Popen(cmd,
+                                 stdout=subprocess.PIPE,
+                                 shell=True
+                                 ).communicate()[0]
+                ).decode('utf-8')
+
+    @staticmethod
+    def _create_get_package_name(root_dir_path: str) -> Callable[[str], bool]:
+        def get_package_name(file_path: str):
+            return file_path.replace(root_dir_path, '').split('/')[0]
+        return get_package_name
+
+    @staticmethod
+    def _ensure_dir_exist(path: str):
+        if(os.path.exists(path)):
+            return
+
+        logger.error('"build" directory not found. '
+                     'Specify the path to the workspace where the build command completed.')
+        exit(1)
+
+    @staticmethod
+    def get_package_name_from_path(root_dir_path: str, path: str) -> str:
+        package_name = path.replace(root_dir_path, '').split('/')[0]
+
+        return package_name

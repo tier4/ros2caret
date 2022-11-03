60305dcce50f2585630952571bdd55698497f3e2
hsgwa <19860128+hsgwa@users.noreply.github.com>
2022-11-04 06:55:09 +0900

    Revert "chore: make force option work without argument (#50)"
    This reverts commit d64956a889e80d07284bb7e1d5bb3559c267594f.

--- ros2caret/verb/create_architecture.py
+++ ros2caret/verb/create_architecture.py
@@ -62,7 +62,7 @@ class CreateArchitectureVerb(VerbExtension):
             required=False, default='./architecture.yaml'
         )
         parser.add_argument(
-            '-f', '--force', dest='force', action='store_true',
+            '-f', '--force', dest='force', type=bool,
             help='allow overwrite of architecture file',
             required=False, default=False
         )

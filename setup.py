506bca21f1513a32e6a19a64b0fb50c93559d1e6
taro-yu <milktea1621@gmai.com>
2023-05-27 18:50:38 +0900

    feat: creat the github actions to create PR and change version info  -s

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
 
+import os
+
 package_name = 'ros2caret'
+ns = dict()
+
+version_path = f'{os.path.dirname(os.path.realpath(__file__))}/ros2caret/__version__.py'
+with open(version_path, 'r') as f:
+   eval(compile(f.read(), version_path, 'exec'), dict(), ns)
 
 setup(
     name=package_name,
-    version='v0.4.8',
+    version=ns['__version__.py'],
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',

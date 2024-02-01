d15c1888ca4793017495ea46d53d71ada07bde76
h-suzuki <h-suzuki@isp.co.jp>
2024-02-01 13:08:32 +0900

    Update: Change in version control method

--- ros2caret/verb/version.py
+++ ros2caret/verb/version.py
@@ -14,27 +14,28 @@
 
 from ros2caret.verb import VerbExtension
 
-# from ..__version__ import __version__
 import os.path
 import codecs
+import re
 
 
 class CaretVersionVerb(VerbExtension):
 
     def main(self, *, args):
-        rel_path = '../../setup.py'
-        version = self.get_version(rel_path)
+        version_path = '../../setup.py'
+        version = self.get_version(version_path)
         print('v' + version)
 
     def read(self,rel_path):
-        here = os.path.abspath(os.path.dirname(__file__))
-        with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        here_path = os.path.dirname(os.path.realpath(__file__))
+        with codecs.open(os.path.join(here_path, rel_path), 'r') as fp:
             return fp.read()
 
     def get_version(self,rel_path):
+        version_pattern = re.compile(r"\s*version\s*=\s*['\"](\d+\.\d+\.\d+)['\"]")
         for line in self.read(rel_path).splitlines():
-            if line.startswith('    version='):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
+            match = version_pattern.search(line)
+            if match:
+                return match.group(1)
         else:
             raise RuntimeError("Unable to find version string.")
